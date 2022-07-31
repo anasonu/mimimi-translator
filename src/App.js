21c08d4e137fe0183e042ed3e18e2f28d1254fce
Ana Sobrino <anasonu@hotmail.com>
2022-07-31 20:00:40 +0200

    Mimimi translator main functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [content, setContent] = useState("");
+
+  const handleContentChange = (e) => setContent(e.target.value);
+  const translateText = (text) => {
+    return text
+      .replaceAll(/[aeou]/g, "i")
+      .replaceAll(/[AEOU]/g, "I")
+      .replaceAll(/[áéóú]/g, "í")
+      .replaceAll(/[ÁÉÓÚ]/g, "Í")
+      .replaceAll(/[äëöü]/g, "ï")
+      .replaceAll(/[ÄËÖÜ]/g, "Ï");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="header">
+        <h1>Traductor MiMiMi</h1>
+      </section>
+      <div className="translator">
+        <div className="translator-container text-container">
+          <h2>Español</h2>
+          <hr className="hr-translator" />
+          <textarea
+            className="text-to-translate text-box"
+            type="text"
+            name="content"
+            onChange={handleContentChange}
+            value={content}
+            placeholder="Escribe aquí para traducir"
+          />
+        </div>
+        <div className="text-container translated-text-container">
+          <h2>MiMiMi</h2>
+          <hr className="hr-translated-text" />
+          <textarea
+            className="text-box translated-text"
+            type="text"
+            name="translation"
+            value={translateText(content)}
+            disabled
+            placeholder="Iscribi iquí piri tridicir"
+          />
+        </div>
+      </div>
     </div>
   );
 }
