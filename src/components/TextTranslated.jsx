cd42fea40dc5a709781a292184934619d58d7d76
Ana Sobrino <anasonu@hotmail.com>
2022-08-01 13:27:24 +0200

    Add copy to clipboard button

--- src/components/TextTranslated.jsx
+++ src/components/TextTranslated.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import CopyIcon from "./CopyIcon";
 
-function TextTranslated({ content }) {
+function TextTranslated({ content, setCopySuccess }) {
   const translateText = (text) => {
     return text
       .replaceAll(/[aeou]/g, "i")
@@ -11,11 +12,28 @@ function TextTranslated({ content }) {
       .replaceAll(/[ÄËÖÜ]/g, "Ï");
   };
 
+  const copyToClipboard = () => {
+    let text = document.getElementById("mimimi-text").textContent;
+    navigator.clipboard.writeText(text);
+
+    setCopySuccess("✅ ¡Texto copiado al portapapeles!");
+
+    setTimeout(() => {
+      setCopySuccess("");
+    }, 2000);
+  };
+
   return (
     <div className="text-container translated-text-container">
-      <h2>MiMiMi</h2>
+      <div className="textbox-header">
+        <h2>MiMiMi</h2>
+        <button className="copy-btn" onClick={copyToClipboard}>
+          <CopyIcon />
+        </button>
+      </div>
       <hr className="hr-translated-text" />
       <textarea
+        id="mimimi-text"
         className="text-box translated-text"
         type="text"
         name="translation"
--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -5,12 +5,17 @@ import TextTranslated from "./TextTranslated";
 
 function Translator() {
   const [content, setContent] = useState("");
+  const [copySuccess, setCopySuccess] = useState("");
+
   return (
     <div>
       <Header />
       <div className="translator">
         <TextToTranslate content={content} setContent={setContent} />
-        <TextTranslated content={content} />
+        <TextTranslated content={content} setCopySuccess={setCopySuccess} />
+      </div>
+      <div className="copy-success-container">
+        <p id="hide-element" className="copy-success-text">{copySuccess}</p>
       </div>
     </div>
   );
